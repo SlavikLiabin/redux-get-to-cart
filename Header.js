b65d9d7fa2bc641bd9ff16e1a167ba8aff447f1f
SlavikLiabin <122288413+SlavikLiabin@users.noreply.github.com>
2024-02-10 13:18:58 +0500

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Badge from "@mui/material/Badge";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Add to Cart</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+
+          <Badge badgeContent={2} color="primary">
+            <i
+              class="fa-sharp fa-solid fa-cart-shopping text-light"
+              style={{ fontSize: 25, cursor: "pointer" }}
+            ></i>
+          </Badge>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
