c866474f123845d2bda46b74299b9a3cec7c7712
SlavikLiabin <122288413+SlavikLiabin@users.noreply.github.com>
2024-02-10 14:14:18 +0500

    Add files via upload

--- Header.js
+++ Header.js
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Badge from "@mui/material/Badge";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="dark" data-bs-theme="dark" style={{ height: "60px" }}>
         <Container>
-          <Navbar.Brand href="#home">Add to Cart</Navbar.Brand>
+          <NavLink to="/" className="text-decoration-none text-light mx-3">
+            Add to Cart
+          </NavLink>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <NavLink to="/" className="text-decoration-none text-light">
+              Home
+            </NavLink>
           </Nav>
 
-          <Badge badgeContent={2} color="primary">
+          <Badge
+            badgeContent={2}
+            color="primary"
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
             <i
               class="fa-sharp fa-solid fa-cart-shopping text-light"
               style={{ fontSize: 25, cursor: "pointer" }}
             ></i>
           </Badge>
         </Container>
+
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <div className="card_details">
+            <p>Your cart is empty</p>
+          </div>
+        </Menu>
       </Navbar>
     </>
   );
